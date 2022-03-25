c023a1eb4bee28ac4f95eae6f73fecefac89d1b8
Paulo Freitas <paulonprinfor@yahoo.com.br>
2022-03-24 23:57:13 -0300

    Criar header do site

--- header.php
+++ header.php
@@ -12,6 +12,34 @@
 </head>
 <body <?php body_class(); ?>>
 <?php global $mydata; ?>
-<header>
-  
+<header class="main-header">
+  <div class="centered-container">
+    <div class="main__header-logo">
+      <a href="<?= esc_url(home_url())?>">
+        <img src="<?=$mydata->header_logo?>" alt="Dr.Nilton">
+      </a>
+    </div>
+    <div class="main-header__inner">
+     <div class="main-header__inner-social">
+        <?php echo $mydata->get_social(true, 'social__list') ?>
+        <div class="form">
+          <?php get_search_form();?>
+        <span class="fa fa-lg fa-search"></span>
+      </div>
+     </div>
+    </div>
+    <nav class="main-menu">
+      <?php
+        $args = array (
+          'theme_location' =>'Menu Principal',
+          'menu' => 'Menu Principal',
+          'container_class' => 'menu-nav-container',
+          'container_id' => '',
+          'menu_class' => 'menu'
+        );
+
+        wp_nav_menu($args);
+      ?>
+    </nav>
+  </div>
 </header>
