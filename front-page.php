40ea7bb7dcc5c3aea209ddd788da094aefd662c2
Paulo Freitas <paulonprinfor@yahoo.com.br>
2022-03-29 21:44:50 -0300

    Criar banner do site

--- front-page.php
+++ front-page.php
@@ -1,4 +1,9 @@
 <?php get_header();?>
-
-  
+  <main class="front-page">
+    <section class="main-banners">
+      <div class="main-banners__slider">
+        <?php get_banners()?>
+      </div>
+    </section>
+  </main>
 <?php get_footer(); ?>
