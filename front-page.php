d85fcbdfacfbb2ebc0682a81a75b5a2072d9ccfd
Paulo Freitas <paulonprinfor@yahoo.com.br>
2022-04-12 23:13:34 -0300

    Criar seção de pre-agendamento na Home

--- front-page.php
+++ front-page.php
@@ -6,4 +6,5 @@
   </section>
   <?php get_template_part('template-parts/article-treatments') ?>
   <?php get_template_part('template-parts/home-page') ?>
+  <?php get_template_part('template-parts/section-schedule') ?>
 <?php get_footer(); ?>
---
+++ template-parts/section-schedule.php
@@ -0,0 +1,15 @@
+<section class="section-schedule ">
+  <div class="centered-container">
+    <div class="section-schedule__title">
+      <img src="<?= get_img_uri() ?>/icn-scheduling.png">
+      <h1>Agende uma consulta!</h1>
+    </div>
+      <div class="section-sechedule__contact">
+        <?php echo do_shortcode( '[contact-form-7 id="89" title="Formulario de contato 2"]' ); ?>
+        <div class="section-sechedule__image">
+          <img src="<?= get_img_uri() ?> /image-scheduling.png" > 
+      </div>
+    </div>
+  </div>
+</section>
+
